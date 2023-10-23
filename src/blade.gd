429d58a1e894961b459189231b77b2a114b8512f
Timo Eberl <timo@timoeberl.de>
2023-10-23 04:19:09 +0200

    Fixed many hits not being recognized when the blade is moving fast

--- src/blade.gd
+++ src/blade.gd
@@ -8,20 +8,28 @@ var base_position_on_enter : Vector3
 var tip_position_on_enter : Vector3
 var time_ms_on_enter : int
 
+
+func _ready():
+	# set the physics framerate very high so fruit hits get registered even with fast blade movement
+	# 60 is default, but even at 120 the results are undesirable
+	Engine.physics_ticks_per_second = 240
+
 func _on_collision_area_entered(_area : Area3D):
 	time_ms_on_enter = Time.get_ticks_msec()
 	base_position_on_enter = base.global_position
 	tip_position_on_enter = tip.global_position
 
-
 func _on_collision_area_exited(area : Area3D):
+	# wait until the blade has moved
+	while tip_position_on_enter == tip.global_position:
+		push_warning("Blade has not moved since it entered. Waiting one physics step.")
+		await get_tree().physics_frame
+	
 	var tip_position_on_exit = tip.global_position
-
-	# determine the normal of the plane on which the 3 recorded blade positions lie
+	
+	# create a plane on which the 3 recorded blade positions lie
 	# this plane represents the cut
-	var normal = get_normal_of_surface_containing_points(
-		tip_position_on_exit, tip_position_on_enter, base_position_on_enter
-	)
+	var plane = Plane(tip_position_on_exit, tip_position_on_enter, base_position_on_enter)
 	
 	var cutting_vector = tip_position_on_exit - tip_position_on_enter
 	var cutting_distance = cutting_vector.length()
@@ -29,12 +37,4 @@ func _on_collision_area_exited(area : Area3D):
 	var cutting_speed = cutting_distance / cutting_time_s
 	var cutting_direction = cutting_vector.normalized()
 
-	area.owner.halve_along_plane(normal, cutting_direction, cutting_speed)
-
-func get_normal_of_surface_containing_points(pos_1 : Vector3, pos_2 : Vector3, pos_3 : Vector3) -> Vector3:
-	# calculate two line segments on the surface
-	var line_seg_1 = pos_1 - pos_2
-	var line_seg_2 = pos_1 - pos_3
-	
-	return line_seg_1.cross(line_seg_2)
-	
+	area.owner.halve_along_plane(plane, cutting_direction, cutting_speed)
--- src/melon.gd
+++ src/melon.gd
@@ -10,13 +10,13 @@ var melon_halve_scene = preload("res://scenes/melon_halve.tscn")
 var debug_plane_scene = preload("res://scenes/debug_plane.tscn")
 
 
-func halve_along_plane(plane_normal : Vector3, cutting_direction : Vector3, cutting_speed : float):
-	if plane_normal.is_zero_approx():
-		push_error("plane_normal can't be zero.")
+func halve_along_plane(plane : Plane, cutting_direction : Vector3, cutting_speed : float):
+	if plane.normal.is_zero_approx():
+		push_error("plane.normal can't be zero.")
 		return
 
-	spawn_halve(plane_normal, cutting_direction, cutting_speed)
-	spawn_halve(-plane_normal, cutting_direction, cutting_speed)
+	spawn_halve(plane.normal, cutting_direction, cutting_speed)
+	spawn_halve(-plane.normal, cutting_direction, cutting_speed)
 
 	self.queue_free()
 
@@ -24,7 +24,7 @@ func halve_along_plane(plane_normal : Vector3, cutting_direction : Vector3, cutt
 		var debug_plane_instance = debug_plane_scene.instantiate()
 		get_tree().get_root().add_child(debug_plane_instance)
 		debug_plane_instance.global_position = self.global_position
-		debug_plane_instance.transform.basis = Basis.looking_at(plane_normal)
+		debug_plane_instance.transform.basis = Basis.looking_at(plane.normal)
 		await get_tree().create_timer(1).timeout
 		debug_plane_instance.queue_free()
 
@@ -37,7 +37,7 @@ func spawn_halve(direction : Vector3, cutting_direction : Vector3, cutting_speed
 
 	var rigid_body : RigidBody3D = melon_halve_instance.get_node("rigidbody")
 	# move away from cutting point
-	rigid_body.apply_central_force(-direction * 150)
+	rigid_body.apply_central_force(-direction * 40)
 	# move in cutting direction
 	rigid_body.apply_central_force(cutting_direction * 10 * cutting_speed)
 
