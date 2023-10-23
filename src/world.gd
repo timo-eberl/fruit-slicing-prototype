cc86b8a8aa8cb9bcae63c1b900ebb76482c787e8
Timo Eberl <timo@timoeberl.de>
2023-10-23 04:19:45 +0200

    Display FPS in window title

--- src/world.gd
+++ src/world.gd
@@ -1,12 +1,16 @@
 extends Node3D
 
-var interface : XRInterface
+@onready var interface : XRInterface = XRServer.find_interface("OpenXR")
+@onready var window_title = get_window().title
 
 func _ready():
-	interface = XRServer.find_interface("OpenXR")
 	if interface and interface.is_initialized():
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized. Is your VR headset connected?")
+
+func _process(_delta):
+	# show fps in window title
+	get_window().title = window_title + " | FPS: " + str(Engine.get_frames_per_second())
